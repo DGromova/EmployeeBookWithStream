4b6a5525fa6277e560e685c589a3ad64ae74f3f3
Daria Gromova <igromova81@gmail.com>
2023-02-28 20:38:02 +0300

    Initial commit

---
+++ src/main/java/com/example/employeebookwithstream/EmployeeBookWithStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeebookwithstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookWithStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookWithStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeebookwithstream/EmployeeBookWithStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeebookwithstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookWithStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
