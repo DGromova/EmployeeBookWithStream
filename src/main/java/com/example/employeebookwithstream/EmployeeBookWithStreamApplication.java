18999dbe1aaf2b7513b8e1ae823fba9339b7a28e
Daria Gromova <igromova81@gmail.com>
2023-02-28 20:38:34 +0300

    First commit

--- src/main/java/com/example/employeebookwithstream/EmployeeBookWithStreamApplication.java
+++ src/main/java/com/example/employeebookwithstream/EmployeeBookWithStreamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeBookWithStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeBookWithStreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookWithStreamApplication.class, args);
+    }
 
 }
